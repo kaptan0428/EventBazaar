7347cccd2bb40aeb4e5b7bd2ea294d2641302b01
kaptan0428 <kaptanpal123098@gmail.com>
2024-10-01 22:51:57 +0530

    First Commit

---
+++ EventBazaar-Backend/src/main/java/com/kaptansingh/EventBazaar/EventBazaarAnEventManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaptansingh.EventBazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBazaarAnEventManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBazaarAnEventManagementServiceApplication.class, args);
+	}
+
+}
